98633ffd37ae00400cc62b7cfc8e30cea3b43118
mkizawa930 <m.kappa930@gmail.com>
2024-04-25 10:38:33 +0900

    update

--- tutorials/mnist/sample.jl
+++ tutorials/mnist/sample.jl
@@ -1,8 +1,32 @@
 using MLDatasets
 using Images
+using Flux
 
-train_X, train_y = MLDatasets.MNIST.traindata(Float32)
 
-Images.load()
+traindata = MLDatasets.MNIST(split=:train) # (; features, targets)
+X_train, y_train = traindata.features, traindata.targets
 
-RGB.(train_X[:,:,1])
+y_train = Flux.onehotbatch(y_train, 0:9)
+
+testdata = MNIST(split=:test)
+X_test, y_test = testdata.features, testdata.targets
+
+model = Chain(
+    Dense(784, 256, relu),
+    Dense(256, 10, relu),
+    softmax
+)
+
+loss(x, y) = Flux.Losses.logitcrossentropy(model(x), y)
+
+optimizer = Adam(0.0001)
+
+parameters = Flux.params(model)
+
+train_data = [( Flux.flatten(X_train), Flux.flatten(y_train) )]
+
+for i in 1:400
+    Flux.train!(loss, parameters, train_data, optimizer)
+end
+
+test_data = [(Flux.flatten(X_test), y_test)]
